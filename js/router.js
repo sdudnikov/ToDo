246b0aba79a584a6e32fee82f53073c237658f34
Sergey <s.dudnikov28@gmail.com>
2015-07-03 07:03:15 -0700

    added link active

--- js/router.js
+++ js/router.js
@@ -1,11 +1,31 @@
 
 
 Todo.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+  this.route('active');
+  });
 });
 
 Todo.TodosRoute = Ember.Route.extend({
   model: function() {
-    return this.store.find('Todos');
+    return this.store.find('todos');  //не зависит от регистра (model name is Todos)
+  }
+});
+
+Todo.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+});
+
+
+Todo.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todos', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
