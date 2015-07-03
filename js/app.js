363e88918cfa6c8a7760a7b94964e97cec77c56a
Sergey <s.dudnikov28@gmail.com>
2015-07-02 23:10:14 -0700

    DISPLAYING MODEL DATA

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
 
 window.Todo = Ember.Application.create();
+
+Todo.ApplicationAdapter = DS.FixtureAdapter.extend();
--- js/router.js
+++ js/router.js
@@ -3,3 +3,9 @@
 Todo.Router.map(function() {
   this.resource('todos', { path: '/' });
 });
+
+Todo.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('Todos');
+  }
+});
